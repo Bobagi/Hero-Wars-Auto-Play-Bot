a2ead58ec992bed019c88f12db6d58f6b26710ba
Gustavo Perin <gustavoperin067@gmail.com>
2023-11-13 03:07:41 -0300

    Adding Descktopmagic to make the screenshots works in windows

--- bots/test.py
+++ bots/test.py
@@ -1,38 +1,32 @@
-import cv2
-import numpy as np
-import pyautogui
+from screeninfo import get_monitors
+from desktopmagic.screengrab_win32 import getRectAsImage, getScreenAsImage
 import os
 import time
-from screeninfo import get_monitors
 
 save_path = 'images/screenshots'
-template_path = 'images/bot/countButton.jpg'
-showImgs = False
+show_images = False
 
 def capture_and_save_screenshots(show_image=False):
     # Create the screenshots folder if it doesn't exist
     if not os.path.exists(save_path):
         os.makedirs(save_path)
+    print("monitors: ",get_monitors())
 
-    for monitor in get_monitors():
-        # Set the current monitor
-        pyautogui.moveTo(monitor.x + 1, monitor.y + 1)
-
+    for i, monitor in enumerate(get_monitors()):
         # Take a screenshot of the current monitor
-        screenshot = pyautogui.screenshot(region=(monitor.x, monitor.y, monitor.width, monitor.height))
-        screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
+        print(monitor.x, monitor.y, monitor.width, monitor.height)
+        screenshot = getRectAsImage((monitor.x, monitor.y, monitor.width, monitor.height))
+        # screenshot = getScreenAsImage()
 
         # Save the screenshot with a timestamp as the filename
-        save_filename = os.path.join(save_path, f"screenshot_{int(time.time())}_monitor{monitor.x}_{monitor.is_primary}.png")
-        cv2.imwrite(save_filename, screenshot)
+        save_filename = os.path.join(save_path, f"screenshot_{int(time.time())}_monitor{i + 1}_{monitor.is_primary}.png")
+        screenshot.save(save_filename)
 
         print(f"Screenshot saved at {save_filename} - Monitor: {monitor.name}")
 
         # Optionally show the screenshot
         if show_image:
-            cv2.imshow('Screenshot', screenshot)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+            screenshot.show()
 
 if __name__ == "__main__":
-    capture_and_save_screenshots()
+    capture_and_save_screenshots(show_images)
